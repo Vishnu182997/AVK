b1684d09a8e8ea18f130331b0dea127f41f190c5
Vishnu <vishnubabukondisetty@gmail.com>
2026-07-12 09:56:31 +0530

    Change

--- common-references/app-common-services/src/main/java/com/example/common/util/AWSConstant.java
+++ common-references/app-common-services/src/main/java/com/example/common/util/AWSConstant.java
@@ -4,18 +4,7 @@ import com.amazonaws.regions.Regions;
 
 public class AWSConstant {
 
-	public static final String CLIENT_ID = "78bat8t5d2fgu1rqo30k3035pc";
-	public static final String POOL_ID = "ap-south-1_Ww3txnMaY";
 	public static final String REGION =Regions.AP_SOUTH_1.getName();
-	public static final String DEVELOPER_GROUP = "developers";
-	public static final String EMAIL = "email";
-	public static final String LOCATION = "custom:country";
-	public static final String CORPORATE_NAME = "custom:Corporate_Name";
-	public static final String GENDER = "gender";
-	public static final String PHONE_NUMBER = "phone_number";
-	public static final String NAME = "name";
-	public static final String ACCESS_KEY = "AKIAYNW2A53R67XSDVNJ";
-	public static final String SECRET_KEY = "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";
 	
 	private AWSConstant() {}
 	
